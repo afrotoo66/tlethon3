a579fb7ed999f8f5b90adac34ffbfbdc8e02c09d
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-01-31 19:46:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from pyrogram import Client,filters,enums
 import redis
 
 r = redis.Redis(
-  host='redis-17811.c323.us-east-1-2.ec2.cloud.redislabs.com',
+  host='redis-18917.c299.asia-northeast1-1.gce.cloud.redislabs.com:18917',
   port=17811,
-  password='y41sFD7N8cY5Ob2MGPZkGdrTndVFY92h')
+  password='clWxuGmyTnMV8Adcf4MTQzuKyFxjRT80')
 
-sudo_id = 6581896306
+sudo_id = 5904216848
 bot_user = Mody.BOT_USER
 via_user = Mody.VIA_USER
 elhyba = bot_user
@@ -16,9 +16,9 @@ api_id = Mody.APP_ID
 api_hash = Mody.API_HASH
 session = Mody.SESSION
 token = Mody.TG_BOT_TOKEN
-sudo_command = [6581896306]
+sudo_command = [5904216848]
 pm =  Mody.MENTION
-mention = "6581896306"
+mention = "5904216848"
 plugins = dict(root="plugins")
 app = Client(via,api_id , api_hash ,in_memory=True,session_string = session,plugins=plugins)
 bot = Client(elhyba,api_id=api_id , api_hash=api_hash ,bot_token=token,plugins=dict(root="plug_bot"))
